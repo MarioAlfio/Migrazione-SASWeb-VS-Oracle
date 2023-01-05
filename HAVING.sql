ce23508e0817a6d8a408fffd526493992f386480
@MarioAlfio <120608309+MarioAlfio@users.noreply.github.com>
2023-01-05 19:19:51 +0100

    Add files via upload

--- HAVING.sql
+++ HAVING.sql
@@ -1,18 +1,33 @@
-/*Quando in SAS ci troviamo una casistica del genere non possiamo andare semplicemente a sostituire la funzione HAVING, 
-perchè la sintassi in SAS è diversa da quella di SQL, quindi abbiamo studiato un modo per tradurre questa funzione. */
+/*Quando in SAS ci troviamo la seguente casistica non possiamo andare semplicemente a sostituire la funzione HAVING, 
+perchè la sintassi in SAS è diversa da quella in SQL, quindi abbiamo studiato un modo per tradurre questa funzione. */
 
 PROC SQL;
 CREATE TABLE TABELLA AS SELECT DISTINCT
-Q.*
-FROM
-TABELLA_RISULTATI Q
+F.*
+FROM TABELLA_PRINCIPALE F
+LEFT JOIN TABELLA_CODES SC ON F.CAMPO_CODE=SC.CAMPO_CODE AND F.CAMPO_CODE=SC.CAMPO_CODE
+WHERE SC.CAMPO='Y'
 GROUP BY
-Q.CAMPO_ID
-HAVING Q.CAMPO_ID=MAX(Q.CAMPO_ID)
+F.CAMPO_ID
+HAVING F.CAMPO_ID=MAX(F.CAMPO_ID)
+;QUIT;
+
+PROC SQL;
+CREATE TABLE TABELLA_1 AS SELECT DISTINCT
+F.*
+FROM TABELLA_PRINCIPALE F
+LEFT JOIN TABELLA_CODES SC ON F.CAMPO_CODE=SC.CAMPO_CODE AND F.CAMPO_CODE=SC.CAMPO_CODE
+WHERE SC.CAMPO='Y'
+GROUP BY
+F.CAMPO_ID
+HAVING F.CAMPO_DATA=MAX(F.CAMPO_DATA)
 ;QUIT;
 
 --In SQL lo tradurremo nel modo seguente:
 
+WITH
+TABELLA AS 
+(
 SELECT * FROM (
     SELECT F.CAMPO_ID,
        	MAX(TRUNC(F.CAMPO_ID)) OVER (PARTITION BY F.CAMPO_TYPE,F.CAMPO_CODE)  AS CAMPO_ID_MAX,
